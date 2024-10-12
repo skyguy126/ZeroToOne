dee292aac104ccf2177d92e1d5e283e53bc79a0b
Vamsi Eyunni <vamsieyunni@gmail.com>
2024-10-12 13:23:31 -0700

    add guid generation

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 'use strict';
 
-// test
+const HTTP_PORT = 9999;
 
 const fs = require('fs');
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+var cookieParser = require('cookie-parser');
 
 const app = express();
-const apiKey = JSON.parse(fs.readFileSync('apikeys.json'));
+const apiKeys = JSON.parse(fs.readFileSync('apikeys.json'));
 const http = require('http').Server(app);
 
-console.log("KindoApi Key:" + apiKey["kindoai"]);
+console.log("KindoApi Key:" + apiKeys["kindoai"]);
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('static'));
+
+//
+// Generate a guid everytime we visit the root page to
+// keep track of the current request. This is what
+// we'll use as the main key in our DB.
+//
+
+app.use(function(req, res, next) {
+    if (req.path === '/' || req.path === '/index.html') {
+        let guid = uuidv4(); // Generate a new GUID
+        res.cookie('guid', guid, { maxAge: 900000, httpOnly: true }); // Set the cookie
+        console.log('New GUID generated and set as cookie:', guid);
+    }
+
+    next();
+});
+
+app.use(express.static('static')); // static file serve.
 
 app.post('/api', function(req, res) {
     let input = req.body;
@@ -22,6 +42,6 @@ app.post('/api', function(req, res) {
     res.end();
 });
 
-http.listen(9999, function() {
-    console.log("Listening on port 9999");
+http.listen(HTTP_PORT, function() {
+    console.log("Listening on port " + HTTP_PORT);
 });
